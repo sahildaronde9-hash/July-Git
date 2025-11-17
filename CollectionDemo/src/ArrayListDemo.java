21341378c1245deb44871db566a7aaba05883675
sahildaronde9@gmail.com <sahildaronde9@gmail.com>
2025-11-17 18:57:39 +0530

    Adding sorted list

---
+++ CollectionDemo/src/ArrayListDemo.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ArrayListDemo {
+
+    public static void main(String[] args){
+
+        List <Integer> list = Arrays.asList(10, 40, 30, 20,70,80);
+
+        System.out.println("Without Sorting>> " +list);
+
+        Collections.sort(list);
+
+        System.out.println("After Sorting>> " +list);
+    }
+}
